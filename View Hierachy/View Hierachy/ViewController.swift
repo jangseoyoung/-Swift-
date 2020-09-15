2125b2bf01838df366c9fc96ef7637f93895a090
jangseoyoung <jangseoyoung0402@gmail.com>
2020-09-15 23:23:46 +0900

    Create & Update : View Hierachy
    뷰 계층 디버깅

---
+++ View Hierachy/View Hierachy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  View Hierachy
+//
+//  Created by 장서영 on 2020/09/15.
+//  Copyright © 2020 장서영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
